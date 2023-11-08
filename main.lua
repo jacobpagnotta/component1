7462961f9fe4f8f76a4faa65b6b386092f711f67
Regaah <regaah.ragunathan@interfaceware.com>
2023-11-08 18:03:08 -0500

    Test1

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
-function main(Data)
-   
+require 'iguana6_shim-deleteme'
+-- This is a very rough example showing how we can generate random HL7
+-- data using the translator.  
+-- http://help.interfaceware.com/v6/random-message-generator
+
+
+local ran = {}
+ran.RandomMessage = require 'ranADT'
+      
+function main()
+   -- Push the ADT message through to destination
+   -- Press 'RandomMessage' on right to navigate
+   -- through code
+   trace{ran.RandomMessage()}
+   queue.push{data=ran.RandomMessage()}
 end
+
