fe19f1994b0ebf34c793362e28b2459c858a54c1
Eliot Muir <eliot.muir@interfaceware.com>
2023-11-08 18:38:53 -0500

    Removing these files.

--- main.lua
+++ main.lua
@@ -1,17 +1,4 @@
-require 'iguana6_shim-deleteme'
--- This is a very rough example showing how we can generate random HL7
--- data using the translator.  
--- http://help.interfaceware.com/v6/random-message-generator
 
+function main(Data)
 
-local ran = {}
-ran.RandomMessage = require 'ranADT'
-      
-function main()
-   -- Push the ADT message through to destination
-   -- Press 'RandomMessage' on right to navigate
-   -- through code
-   trace{ran.RandomMessage()}
-   queue.push{data=ran.RandomMessage()}
 end
-
